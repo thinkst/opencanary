9fd6e77b3cad363636369d91feb41676e8375cc7
Michael Van Delft <michael@hybr.id.au>
2019-02-07 20:32:55 +0800

    Bumped requirements to the latest version
    Bumped all of the required packages up to their latest version and added
    wsgiref separately just for Python 2

--- setup.py
+++ setup.py
@@ -1,7 +1,24 @@
+import sys
 from setuptools import setup, find_packages
 
-import os
 import opencanary
+
+requirements = [
+    'Twisted==18.9.0',
+    'pyasn1==0.4.5',
+    'pycrypto==2.6.1',
+    'simplejson==3.16.0',
+    'zope.interface==4.6.0',
+    'PyPDF2==1.26.0',
+    'fpdf==1.7.2',
+    'passlib==1.7.1',
+    'Jinja2==2.10.0',
+    'ntlmlib==0.72']
+
+# Python 2 requires wsgiref but with python 3 wsgiref is a standard library.
+if sys.version_info[0] < 3:
+    requirements.append('wsgiref==0.1.2')
+
 
 setup(
     name='opencanary',
@@ -11,26 +28,13 @@ setup(
     author_email='info@thinkst.com',
     description='OpenCanary daemon',
     long_description='A low interaction honeypot intended to be run on internal networks.',
-    install_requires=[
-        'Twisted==14.0.2',
-        'pyasn1==0.1.7',
-        'pycrypto==2.6.1',
-        'simplejson==3.6.5',
-        'wsgiref==0.1.2',
-        'zope.interface==4.1.1',
-        'PyPDF2==1.23',
-        'fpdf==1.7',
-        'passlib==1.6.2',
-        'Jinja2>=2.4',
-        'ntlmlib==0.67'
-    ],
+    install_requires=requirements,
     setup_requires=[
         'setuptools_git'
     ],
     license='BSD',
-    packages = find_packages(exclude='test'),
+    packages=find_packages(exclude='test'),
     scripts=['bin/opencanaryd','bin/opencanary.tac'],
     platforms='any',
     include_package_data=True
 )
-
