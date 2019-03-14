f928425dc4abfd1458aef9a3e0cf8459ce709375
Jason Bissict <jason@thinkst.com>
2019-03-14 10:11:04 +0200

    Bump version of dependencies and OpenCanary version

--- setup.py
+++ setup.py
@@ -12,18 +12,18 @@ setup(
     description='OpenCanary daemon',
     long_description='A low interaction honeypot intended to be run on internal networks.',
     install_requires=[
-        'Twisted==14.0.2',
-        'pyasn1==0.1.7',
-        'pycrypto==2.6.1',
-        'simplejson==3.6.5',
-        'wsgiref==0.1.2',
-        'zope.interface==4.1.1',
-        'PyPDF2==1.23',
-        'fpdf==1.7',
-        'passlib==1.6.2',
         'Jinja2>=2.4',
-        'ntlmlib==0.67',
+        'Twisted==18.4.0',
+        'pyasn1==0.4.5',
+        'pycrypto==2.6.1',
+        'simplejson==3.16.0',
+        'wsgiref==0.1.2',
         'requests==2.7.0'
+        'zope.interface==4.4.2',
+        'PyPDF2==1.26.0',
+        'fpdf==1.7.2',
+        'passlib==1.7.1',
+        'ntlmlib==0.71'
     ],
     setup_requires=[
         'setuptools_git'
