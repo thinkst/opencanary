55243972a90db3ed3be767b6e76c7e1dc4cf2f6a
Joubin Jabbari <joubin@jabbari.io>
2020-11-10 22:38:10 -0800

    Adding two different docker builds

--- run.sh
+++ run.sh
@@ -5,10 +5,10 @@ TEMP_CONF="/root/.opencanary.conf"
 
 if [ -f $CONF ]; then
 	echo "INFO: Main configuration file found"
-	opencanaryd --start && tail -f /var/tmp/opencanary.log
+	opencanaryd --dev
 elif [ -f $TEMP_CONF ]; then
 	echo "INFO: Temp configuration file found"
-	opencanaryd --start && tail -f /var/tmp/opencanary.log
+	opencanaryd --dev
 else
 	opencanaryd --copyconfig && echo "A Config file was generated at /root/.opencanary.conf. If the volume is mapped, you have a copy and should move it to /root/opencanary.conf" && echo "In either case, you can rerun this container and have it run."
 fi
