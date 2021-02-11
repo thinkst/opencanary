2768c4990c9e643dde2aaf3cec2666809a530af4
NewNeonNeo <enmainvi01@monccnb.ca>
2021-02-10 22:37:25 -0400

    Update run.sh
    If statement now runs --start/--dev. Fixed instructions related to config file location.

--- run.sh
+++ run.sh
@@ -5,11 +5,11 @@ TEMP_CONF="/root/.opencanary.conf"
 
 if [ -f $CONF ]; then
 	echo "INFO: Main configuration file found"
-	opencanaryd --dev
+	opencanaryd --start
 elif [ -f $TEMP_CONF ]; then
 	echo "INFO: Temp configuration file found"
 	opencanaryd --dev
 else
-	opencanaryd --copyconfig && echo "A Config file was generated at /root/.opencanary.conf. If the volume is mapped, you have a copy and should move it to /root/opencanary.conf" && echo "In either case, you can rerun this container and have it run."
+	opencanaryd --copyconfig && echo "A Config file was generated at /etc/opencanaryd/.opencanary.conf. If the volume is mapped, move it to /root/opencanary.conf"
 fi
 
