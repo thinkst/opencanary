9d8f5fa07cb65d568c40aaf44fe3a9d0b8b42ecc
Jason Bissict <jason@thinkst.com>
2022-06-29 12:11:10 +0200

    .

--- opencanary/modules/git.py
+++ opencanary/modules/git.py
@@ -62,7 +62,7 @@ class GitProtocol(Protocol):
                 else:
                     raise ProtocolError()
 
-            except GitCommandLengthMismatch():
+            except GitCommandLengthMismatch:
                 pass
 
         except ProtocolError:
