d483ecee4411818a478705ee0ad1866d5af4d9ff
Michael Van Delft <michael@hybr.id.au>
2023-03-02 14:09:14 +0800

    Import classes from http module to reduce duplicate code

--- opencanary/modules/https.py
+++ opencanary/modules/https.py
@@ -15,153 +15,13 @@ from twisted.web.util import Redirect
 from twisted.internet.ssl import DefaultOpenSSLContextFactory
 
 from opencanary.modules import CanaryService
-from opencanary.modules.http import CanaryHTTP
-
-
-class Error(Resource):
-    isLeaf = True
-
-    def __init__(self, factory, error_code="404"):
-        self.factory = factory
-        self.skin = self.factory.skin
-        self.skindir = self.factory.skindir
-        self.error_code = error_code
-
-        if not os.path.isdir(self.skindir):
-            raise Exception(
-                "Directory %s for http skin, %s, does not exist."
-                % (self.skindir, self.skin)
-            )
-
-        with open(os.path.join(self.skindir, error_code + ".html")) as f:
-            self.error_contents = f.read()
-
-        Resource.__init__(self)
-
-    def err_page(self, request):
-        path = request.path.replace(b"<", b"&lt;").replace(b">", b"&gt;")
-        return self.error_contents.replace("[[URL]]", path.decode("utf-8")).replace(
-            "[[BANNER]]", self.factory.banner.decode("utf-8")
-        )
-
-    def render(self, request):
-        request.setHeader(b"Server", self.factory.banner)
-        request.setResponseCode(int(self.error_code))
-        return Resource.render(self, request)
-
-    def render_GET(self, request):
-        return self.err_page(request).encode()
-
-    def render_POST(self, request):
-        return self.err_page(request).encode()
-
-
-class BasicLogin(Resource):
-    isLeaf = True
-
-    def __init__(self, factory):
-        self.factory = factory
-        self.skin = self.factory.skin
-        self.skindir = self.factory.skindir
-
-        if not os.path.isdir(self.skindir):
-            raise Exception(
-                "Directory %s for http skin, %s, does not exist."
-                % (self.skindir, self.skin)
-            )
-
-        with open(os.path.join(self.skindir, "index.html")) as f:
-            text = f.read()
-
-        p = re.compile(r"<!--STARTERR-->.*<!--ENDERR-->", re.DOTALL)
-        self.login = re.sub(p, "", text)
-        self.err = re.sub(r"<!--STARTERR-->|<!--ENDERR-->", "", text)
-        Resource.__init__(self)
-
-    def render(self, request):
-        request.setHeader("Server", self.factory.banner)
-        return Resource.render(self, request)
-
-    def render_GET(self, request, loginFailed=False):
-        if not loginFailed:
-            us = request.transport.getHost()
-            peer = request.transport.getPeer()
-            useragent = request.getHeader("user-agent")
-            if not useragent:
-                useragent = "<not supplied>"
-
-            logdata = {
-                "SKIN": self.skin,
-                "HOSTNAME": request.getRequestHostname(),
-                "PATH": request.path,
-                "USERAGENT": useragent,
-            }
-
-            logtype = self.factory.logger.LOG_HTTP_GET
-            self.factory.log(logdata, transport=request.transport, logtype=logtype)
-
-        return self.login.encode()
-
-    def render_POST(self, request):
-        try:
-            username = request.args[b"username"][0]
-        except (KeyError, IndexError):
-            username = "<not supplied>"
-        try:
-            password = request.args[b"password"][0]
-        except (KeyError, IndexError):
-            password = "<not supplied>"
-        useragent = request.getHeader(b"user-agent")
-        if not useragent:
-            useragent = "<not supplied>"
-
-        logdata = {
-            "USERNAME": username,
-            "PASSWORD": password,
-            "SKIN": self.skin,
-            "HOSTNAME": request.getRequestHostname(),
-            "PATH": request.path,
-            "USERAGENT": useragent,
-        }
-        logtype = self.factory.logger.LOG_HTTP_POST_LOGIN_ATTEMPT
-        self.factory.log(logdata, transport=request.transport, logtype=logtype)
-
-        return self.err.encode()
-
-
-class RedirectCustomHeaders(Redirect):
-    def __init__(self, request, factory):
-        Redirect.__init__(self, request)
-        self.factory = factory
-
-    def render(self, request):
-        request.setHeader(b"Server", self.factory.banner)
-        return Redirect.render(self, request)
-
-
-class StaticNoDirListing(static.File):
-    """Web resource that serves static directory tree.
-
-    Directory listing is not allowed, and custom headers are set.
-
-    """
-
-    # Banner is declared statically because Twisted creates several
-    # instances of this object, where we don't control the arguments
-    # for the initialisation.
-    BANNER = None
-    FORBIDDEN = None
-
-    def createErrorPages(self, factory):
-        self.childNotFound = Error(factory, error_code="404")
-        StaticNoDirListing.FORBIDDEN = Error(factory, error_code="403")
-
-    def directoryListing(self):
-        return StaticNoDirListing.FORBIDDEN
-
-    def getChild(self, name, request):
-        request.setHeader("Server", StaticNoDirListing.BANNER)
-        return static.File.getChild(self, name, request)
+from opencanary.modules.http import (
+    BasicLogin,
+    CanaryHTTP,
+    Error,
+    RedirectCustomHeaders,
+    StaticNoDirListing,
+)
 
 
 class CanaryHTTPS(CanaryService):
@@ -242,7 +102,6 @@ class CanaryHTTPS(CanaryService):
             # Write our certificate out to disk.
             with open(self.certificate_path, "wb") as f:
                 f.write(cert.public_bytes(serialization.Encoding.PEM))
-
 
     def getService(self):
         page = BasicLogin(factory=self)
