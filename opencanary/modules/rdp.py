d8d78ad0fe8d936195d3118e12c76e1880c53c90
Michael Van Delft <michael@hybr.id.au>
2023-04-18 20:50:57 +0800

    Update RDP to be a simple service that always disconnects with an error.

--- opencanary/modules/rdp.py
+++ opencanary/modules/rdp.py
@@ -1,96 +1,42 @@
-from twisted.internet import reactor
-from twisted.internet.protocol import Protocol
-from twisted.internet.protocol import Factory
-from twisted.application import internet
-from rdpy.protocol.rdp.rdp import RDPServerObserver
-from rdpy.protocol.rdp.rdp import ServerFactory
-from rdpy.core import rss
-from rdpy.core.scancode import scancodeToChar
+import re
+
 from opencanary.modules import CanaryService
 
-class RDPObserver(RDPServerObserver):
-    def __init__(self, factory, controller, rssFile):
-        RDPServerObserver.__init__(self, controller)
-        self.factory = factory
-        self.buffer = ""
+from twisted.internet.protocol import Protocol
+from twisted.internet.protocol import Factory
 
-    def onReady(self):
-        domain, username, password = self._controller.getCredentials()
-        hostname = self._controller.getHostname()
-        logdata = {
-            "DOMAIN": domain,
-            "USERNAME": username,
-            "PASSWORD": password,
-            "HOSTNAME": hostname
-        }
-        transport = self._controller.getProtocol().transport
-        us = transport.getHost()
-        peer = transport.getPeer()
-        self.transportlog = {
-            'src_host' : peer.host,
-            'src_port' : peer.port,
-            'dst_host' : us.host,
-            'dst_port' : us.port
-        }
-        self.factory.log(logdata, **self.transportlog)
-        self.doEvent(0)
 
-    def onClose(self):
-        if getattr(self, "transportlog", None):
-            logdata = {"INPUT" : self.buffer}
-            self.factory.log(logdata, **self.transportlog)
+class RemoteDesktopProtocol(Protocol):
+    """
+    A simple service that logs RDP connection attempts.
+    Does not implement Network Level Authentication (NLA)
+    """
 
-    def onKeyEventScancode(self, code, isPressed, isExtended):
-        self.buffer += scancodeToChar(code)
+    def dataReceived(self, data):
+        # Decode the data to unicode, so we can search it, and ignore any errors
+        # caused by bytes that can't be decoded.
+        decoded_data = data.decode(encoding="utf-8", errors="ignore")
+        # Use regex to extract the username.
+        match = re.search(r"mstshash=(?P<username>[a-zA-Z0-9-_@]*)", decoded_data)
+        username = match and match.groupdict().get("username")
 
-    def onKeyEventUnicode(self, code, isPressed):
-        pass
+        # Log the connection attempt
+        self.factory.log(logdata={"USERNAME": username}, transport=self.transport)
 
-    def onPointerEvent(self, x, y, button, isPressed):
-        pass
+        # Always respond with a negotiation failure
+        # https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-rdpbcgr/1b3920e7-0116-4345-bc45-f2c4ad012761
+        self.transport.write(b"0x3 RDP_NEG_FAILURE")
+        self.transport.loseConnection()
 
-    def doEvent(self, i):
-        if i >= len(self.factory.rss) - 1:
-            return
 
-        e = self.factory.rss[i]
-
-        # TODO: handle other events (eg. the screen resize)
-        if e.type.value == rss.EventType.UPDATE:
-            self._controller.sendUpdate(
-                e.event.destLeft.value,
-                e.event.destTop.value,
-                e.event.destRight.value,
-                e.event.destBottom.value,
-                e.event.width.value, e.event.height.value,
-                e.event.bpp.value,
-                e.event.format.value == rss.UpdateFormat.BMP,
-                e.event.data.value)
-
-        t = self.factory.rss[i+1].timestamp.value
-        reactor.callLater(float(t) / 1000.0, self.doEvent, i + 1)
-
-class CanaryRDP(ServerFactory, CanaryService):
-    NAME = 'rdp'
+class CanaryRDP(Factory, CanaryService):
+    NAME = "rdp"
+    protocol = RemoteDesktopProtocol
 
     def __init__(self, config=None, logger=None):
-        ServerFactory.__init__(self, 16, None, None)
         CanaryService.__init__(self, config, logger)
-
-        self.rssFile = self.resource_filename("login.rss")
-        reader = rss.createReader(self.rssFile)
-        self.rss = []
-        while True:
-            e = reader.nextEvent()
-            if e:
-                self.rss.append(e)
-            else:
-                break
-
         self.port = config.getVal("rdp.port", 3389)
         self.logtype = logger.LOG_RDP
 
-    def buildObserver(self, controller, addr):
-        return RDPObserver(self, controller, self.rssFile)
 
 CanaryServiceFactory = CanaryRDP
