2a240e43d4cdaf600d62b1d5e8c531606529f8a9
thinkst-az <156116192+thinkst-az@users.noreply.github.com>
2024-09-12 10:36:09 +0200

    Fix lint errors (#370)

--- build_scripts/generate_macOS_launchctl_service_files.py
+++ build_scripts/generate_macOS_launchctl_service_files.py
@@ -20,9 +20,9 @@ OPENCANARY = "opencanary"
 LAUNCH_DAEMONS_DIR = "/Library/LaunchDaemons"
 DEFAULT_SERVICE_NAME = "com.thinkst.opencanary"
 CONFIG_FILE_BASENAME = "opencanary.conf"
-DEFAULT_CONFIG_DIR = importlib.resources.files(OPENCANARY).joinpath('data')
-USER_CONFIG_FILE = DEFAULT_CONFIG_DIR.joinpath('settings.json')
-DEFAULT_CONFIG_FILE = DEFAULT_CONFIG_DIR.joinpath('.opencanary.conf')
+DEFAULT_CONFIG_DIR = importlib.resources.files(OPENCANARY).joinpath("data")
+USER_CONFIG_FILE = DEFAULT_CONFIG_DIR.joinpath("settings.json")
+DEFAULT_CONFIG_FILE = DEFAULT_CONFIG_DIR.joinpath(".opencanary.conf")
 
 # opencanary dirs
 OPENCANARY_DIR = realpath(join(dirname(__file__), pardir))
@@ -52,13 +52,17 @@ if USER_CONFIG_FILE.exists():
     opencanary_config_file = USER_CONFIG_FILE
 else:
     if not DEFAULT_CONFIG_FILE.exists():
-        print(f"Neither a user 'settings.json' nor a default '.opencanary.conf' found in '{DEFAULT_CONFIG_DIR}'!")
+        print(
+            f"Neither a user 'settings.json' nor a default '.opencanary.conf' found in '{DEFAULT_CONFIG_DIR}'!"
+        )
         print("Exiting...")
         sys.exit()
 
     opencanary_config_file = DEFAULT_CONFIG_FILE
-    print(f"Using default config file......")
-    print(f"(Create a file at '{USER_CONFIG_FILE}' for individual settings beyond the command line arguments)")
+    print("Using default config file......")
+    print(
+        f"(Create a file at '{USER_CONFIG_FILE}' for individual settings beyond the command line arguments)"
+    )
 
 print(f"\nUsing base configuration file: '{opencanary_config_file}'")
 
@@ -210,5 +214,7 @@ print(f"       Launcher script: ./{path.relpath(launcher_script)}")
 print(f"      Bootstrap script: ./{path.relpath(install_service_script)}")
 print(f"        Bootout script: ./{path.relpath(uninstall_service_script)}\n")
 print(f"                Config: ./{path.relpath(config_output_file)}")
-print(f"      Enabled canaries: {', '.join(args.canaries) if args.canaries else 'Nothing enabled!'}\n")
+print(
+    f"      Enabled canaries: {', '.join(args.canaries) if args.canaries else 'Nothing enabled!'}\n"
+)
 print(f"To install as a system service run:\n    'sudo {install_service_script}'\n")
