b7b58f204f1e8d2b03d61937b510fca3091f286c
Azhar Desai <azhar@thinkst.com>
2024-09-12 10:24:02 +0200

    fix black

--- opencanary/modules/llmnr.py
+++ opencanary/modules/llmnr.py
@@ -8,17 +8,24 @@ from scapy.layers.dns import DNSQR
 from scapy.layers.llmnr import LLMNRQuery
 import random
 
+
 class LLMNR(DatagramProtocol):
     def startQueryLoop(self):
-            self.sendLLMNRQuery()
-            next_interval = self.factory.query_interval + random.uniform(-self.factory.query_splay, self.factory.query_splay)
-            reactor.callLater(next_interval, self.startQueryLoop)
+        self.sendLLMNRQuery()
+        next_interval = self.factory.query_interval + random.uniform(
+            -self.factory.query_splay, self.factory.query_splay
+        )
+        reactor.callLater(next_interval, self.startQueryLoop)
 
     def sendLLMNRQuery(self):
         # Craft an LLMNR query packet
         target_ip = "224.0.0.252"  # LLMNR multicast IP address
-        target_port = 5355         # LLMNR port
-        llmnr_packet = IP(dst=target_ip, ttl=1) / UDP(dport=target_port) / LLMNRQuery(qd=DNSQR(qname=self.factory.query_hostname))
+        target_port = 5355  # LLMNR port
+        llmnr_packet = (
+            IP(dst=target_ip, ttl=1)
+            / UDP(dport=target_port)
+            / LLMNRQuery(qd=DNSQR(qname=self.factory.query_hostname))
+        )
 
         # Send the packet, set verbose to False to suppress "Sent 1 packet" std-out
         send(llmnr_packet, verbose=False)
@@ -27,29 +34,43 @@ class LLMNR(DatagramProtocol):
         try:
             llmnr_response = DNS(data)
             # Decode bytes to string and remove trailing dot if present
-            received_hostname = llmnr_response.qd.qname.decode('utf-8').rstrip('.')
+            received_hostname = llmnr_response.qd.qname.decode("utf-8").rstrip(".")
             # If the received hostname matches the canary hostname, it's suspicous - log it
             if received_hostname == self.factory.query_hostname:
                 source_ip = host_and_port[0]
-                logdata = {"query_hostname": self.factory.query_hostname, "response": llmnr_response.summary()}
-                self.transport.getPeer = lambda: IPv4Address("UDP", source_ip, host_and_port[1])
-                self.factory.log(logdata=logdata, transport=self.transport, logtype=self.factory.logtype_query_response)
+                logdata = {
+                    "query_hostname": self.factory.query_hostname,
+                    "response": llmnr_response.summary(),
+                }
+                self.transport.getPeer = lambda: IPv4Address(
+                    "UDP", source_ip, host_and_port[1]
+                )
+                self.factory.log(
+                    logdata=logdata,
+                    transport=self.transport,
+                    logtype=self.factory.logtype_query_response,
+                )
 
         except Exception as e:
-                error_message = f"Error processing LLMNR response: {e}"
-                self.factory.log(error_message, level='error')
+            error_message = f"Error processing LLMNR response: {e}"
+            self.factory.log(error_message, level="error")
+
 
 class CanaryLLMNR(CanaryService):
-    NAME = 'llmnr'
+    NAME = "llmnr"
 
     def __init__(self, config=None, logger=None):
         super(CanaryLLMNR, self).__init__(config=config, logger=logger)
         self.logtype_query_response = logger.LOG_LLMNR_QUERY_RESPONSE
-        self.query_hostname = config.getVal('llmnr.hostname', default='DC03')
-        self.port = int(config.getVal('llmnr.port', default=5355))
-        self.query_interval = int(config.getVal('llmnr.query_interval', default=60))  # Interval in seconds
-        self.query_splay = int(config.getVal('llmnr.query_splay', default=5))  # Default splay in seconds
-        self.listen_addr = config.getVal('device.listen_addr', default='')
+        self.query_hostname = config.getVal("llmnr.hostname", default="DC03")
+        self.port = int(config.getVal("llmnr.port", default=5355))
+        self.query_interval = int(
+            config.getVal("llmnr.query_interval", default=60)
+        )  # Interval in seconds
+        self.query_splay = int(
+            config.getVal("llmnr.query_splay", default=5)
+        )  # Default splay in seconds
+        self.listen_addr = config.getVal("device.listen_addr", default="")
 
     def getService(self):
         f = LLMNR()
