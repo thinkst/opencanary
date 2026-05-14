1041fe186be426bddaa61beb7fbc3144c3b95f49
Pieter Smit <pieter@thinkst.com>
2026-05-14 21:21:22 +0200

    black formatting

--- opencanary/test/test_git.py
+++ opencanary/test/test_git.py
@@ -3,14 +3,17 @@ import git
 
 from helpers import get_last_log
 
+
 @pytest.fixture
 def git_repo():
     repo = git.Repo
     yield repo
 
+
 def test_clone_a_repository(git_repo):
     with pytest.raises(git.exc.GitCommandError):
         git_repo.clone_from("git://localhost/test.git", "/tmp/git_test")
+
 
 def test_log_git_clone(git_repo):
     """
